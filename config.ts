1fd8972581a1fc28efd0d36faf6bcfdef568ceca
hashnj <joshinikhilesh46@gmail.com>
2025-03-09 07:56:11 +0530

    one

--- config.ts
+++ config.ts
@@ -1,6 +1,9 @@
-export const URI = 'mongodb+srv://hashnj:Njoshi%40%2345@cluster0.qhvhkeg.mongodb.net/proj?retryWrites=true&w=majority'
-// export const URI = 'mongodb+srv://nikhilesh:Njoshi%40%2345@proj.zzz6y.mongodb.net/proj?retryWrites=true&w=majority'
-export const JwtSecret = 'secret';
-export const F_URL = 'http://localhost:5173';
-export const B_PORT = 3300; 
-export const STRIPE_SECRET_KEY = 'sk_test_51QybhfRufLUkLRT0z7CDDeGx3ezr8cMU5ZonPw88i836OdhvwOjOEZYs6prdP3WJJF6nQ0VbO48zcEaBT3AnA7ix0028XlEe4o';
+import dotenv from "dotenv";
+
+dotenv.config(); 
+
+export const URI = process.env.MONGO_URI || "";
+export const JwtSecret = process.env.JWT_SECRET || "default_secret";
+export const F_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const B_PORT = process.env.BACKEND_PORT ? Number(process.env.BACKEND_PORT) : 3300;
+export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET || "";
