4051fd0f07423c3ba1c5d0eac3c24fc90cd4d4b9
hashnj <joshinikhilesh46@gmail.com>
2025-03-09 09:09:28 +0530

    fINAL

--- config.ts
+++ config.ts
@@ -4,6 +4,6 @@ dotenv.config();
 
 export const URI = process.env.MONGO_URI || "";
 export const JwtSecret = process.env.JWT_SECRET || "default_secret";
-export const F_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const F_URL = process.env.FRONTEND_URL || "https://corecart.vercel.app";
 export const B_PORT = process.env.BACKEND_PORT ? Number(process.env.BACKEND_PORT) : 3300;
 export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET || "";
