e6e5d8edb44f12d4592ad585a295898b5c898d95
hashnj <joshinikhilesh46@gmail.com>
2025-03-09 09:53:01 +0530

    fINAL

--- index.ts
+++ index.ts
@@ -1,47 +1,56 @@
-import express from 'express'
+import express from 'express';
 import { connectDB } from './db';
-import authRouter from './routes/auth'
-import { B_PORT, F_URL } from './config';
+import authRouter from './routes/auth';
+import { B_PORT } from './config';
 import ProductRouter from './routes/product';
 import categoryRouter from './routes/category';
 import OrderRouter from './routes/order';
 import ReviewRouter from './routes/review';
 
 import cors from 'cors';
-import cookieParser from "cookie-parser";
+import cookieParser from 'cookie-parser';
 import CartRouter from './routes/cart';
 import wishlistRouter from './routes/wishList';
 
-const app=express();
+const app = express();
 
 connectDB();
 
-app.use(cookieParser());
+const allowedOrigins = [
+  'http://localhost:5173', // Local frontend
+  'https://corecart.vercel.app', // Production frontend
+];
+
 app.use(cors({
-  origin: F_URL,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 }));
+
+app.use(cookieParser());
 app.use(express.json());
 
-app.use('/auth',authRouter)
-app.use('/product',ProductRouter)
-app.use('/cart',CartRouter)
-app.use('/category',categoryRouter)
-app.use('/wishlist',wishlistRouter)
-app.use('/reviews',ReviewRouter)
-app.use('/order',OrderRouter)
+app.use('/auth', authRouter);
+app.use('/product', ProductRouter);
+app.use('/cart', CartRouter);
+app.use('/category', categoryRouter);
+app.use('/wishlist', wishlistRouter);
+app.use('/reviews', ReviewRouter);
+app.use('/order', OrderRouter);
 
-
-
-
+// Global error handler
 app.use((err: any, _req: express.Request, res: express.Response, next: express.NextFunction) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
 });
 
-const PORT = B_PORT ;
-
+const PORT = B_PORT;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
